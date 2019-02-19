9b269dcd4302ef7c9a724a5718e7c4618e19fe36
Andrea Veneziano <andrea.veneziano@epfl.ch>
2019-02-19 11:46:12 +0100

    Add checkParens complete implementation and related tests

--- dependent-regexp/CheckParens.scala
+++ dependent-regexp/CheckParens.scala
@@ -1,10 +1,33 @@
-def checkParens(s: List[Char]): Boolean = {
-    def check(cs: List[Char], opened: Int): Boolean = cs match {
-        case Nil => opened == 0
-        case '(' :: xs => check(xs, opened + 1)
-        case ')' :: _ if opened < 1 => false
-        case ')' :: xs => check(xs, opened - 1)
-        case _ :: xs => check(xs, opened)
+object ListIntConcat {
+  sealed trait List {
+    dependent def ++(that: List): List =
+      if (this.isInstanceOf[Nil.type]) that
+      else Cons(this.asInstanceOf[Cons].head, this.asInstanceOf[Cons].tail ++ that)
+  }
+  dependent case object Nil extends List
+  dependent case class Cons(head: Int, tail: List) extends List
+}
+
+object DependentRegexp {
+    import ListIntConcat._
+
+    dependent private def check(cs: List, opened: Int): Boolean = {
+        if (cs.isInstanceOf[Nil.type]) opened == 0
+        else if (cs.asInstanceOf[Cons].head == '(') check(cs.asInstanceOf[Cons].tail, opened + 1)
+        else if (cs.asInstanceOf[Cons].head == ')' && opened < 1) false
+        else if (cs.asInstanceOf[Cons].head == ')') check(cs.asInstanceOf[Cons].tail, opened - 1)
+        else check(cs.asInstanceOf[Cons].tail, opened)
     }
-    check(s, 0)
+
+    // Dependent function definition inside another function doesn't work
+    dependent def checkParens(s: List): Boolean = {
+        check(s, 0)
+    }
+
+    val balancedEmpty: true = checkParens(Nil)
+    val balanced1: true = checkParens(Cons('(', Cons('(', Cons(')', Cons(')', Nil)))))
+    val balanced2: true = checkParens(Cons('a', Cons('(', Cons('s', Cons('(', Cons('v', Cons('v', Cons(')', Cons('d', Cons(')', Cons('f', Cons('f', Nil))))))))))))
+
+    val notBalanced1: false = checkParens(Cons('(', Cons('(', Cons(')', Nil))))
+    val notBalanced2: false = checkParens(Cons('(', Cons('(', Cons(')', Cons(')', Cons(')', Cons('(', Nil)))))))
 }
