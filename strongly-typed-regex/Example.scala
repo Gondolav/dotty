58a794bf699b145491581c8d03be46b5bc69a8f6
Andrea Veneziano <andrea.veneziano@epfl.ch>
2019-05-31 11:13:44 +0200

    Update Scala examples

--- strongly-typed-regex/Example.scala
+++ strongly-typed-regex/Example.scala
@@ -5,7 +5,8 @@ object Example {
 
   val regexURL: Regex = "(https?://)?(www.)?([a-z][a-z]*)(.)([a-z][a-z][a-z]*)(/[a-z]*)*".r
   val url: Option[URL] = "https://www.epfl.ch/schools/ic/" match {
-    case regexURL(protocol, _, hostname, _, domain, path) => Some(URL(Option(protocol), hostname, domain, Option(path))) // here if an optional is not present, its value is null
+    // here if an optional is not present, its value is null
+    case regexURL(protocol, _, hostname, _, domain, path) => Some(URL(Option(protocol), hostname, domain, Option(path))) // since the Regex extractor produces varargs, the user could use the wrong group without any safety check
     case _ => None
   }
 
@@ -22,8 +23,8 @@ object Example {
   //---------------------------------------------------------------------------------
 
   val regexCompilationResult: Regex = "(Total time: )([0-9][0-9]*)( s, completed )([A-Z][a-z]*)( )([0-9][0-9]?)(, )([0-9][0-9][0-9][0-9])( )([0-9][0-9]?)(:)([0-9][0-9])(:)([0-9][0-9])( )([A-Z][A-Z])".r
-  val compilationTime: Int = "Total time: 228 s, completed May 9, 2019 7:33:37 PM" match {
-    case regexCompilationResult(_, time, x: _*) => time.toInt // if we call toInt on the wrong group, we get an exception at run-time
-    case _ => -1
+  val compilationTime: Option[Int] = "Total time: 228 s, completed May 9, 2019 7:33:37 PM" match {
+    case regexCompilationResult(_, time, x: _*) => Some(time.toInt) // if we call toInt on the wrong group, we get an exception at run-time
+    case _ => None
   }
 }
